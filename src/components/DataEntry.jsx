4670e629f8ff4289152313d65ab2e984882f8138
dmedinas <dmedinas@gmail.com>
2024-06-03 11:45:23 -0500

    Mejorado responsive de los inputs de entrada de datos

--- src/components/DataEntry.jsx
+++ src/components/DataEntry.jsx
@@ -1,65 +1,63 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../context/Context'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { allergies } from '../data/allergies'
 import { dietType } from '../data/dietType'
 import { useChecked } from '../hooks/useChecked'
-import Toastify from 'toastify-js'
 import { useDietPlan } from '../hooks/useDietPlan'
 import { useDietParams } from '../hooks/useDietParams'
 const DataEntry = () => {
-const[minCalories, setMinCalories] = useState('')
-const[maxCalories, setMaxCalories] = useState('')
-const {setMealsID} =  useContext(Context)
-const [checkedIDDiet, setCheckedIDDiet] = useState([])
-const [checkedIDAllergies, setCheckedIDAllergies] = useState([])
-const dietHealth = useChecked({checkedType:checkedIDDiet, checkedList:dietType})
-const allergiesCare = useChecked({checkedType:checkedIDAllergies,checkedList:allergies})
-const {params,error} = useDietParams({minCalories, maxCalories,dietHealth,allergiesCare})
-const {planResult} = useDietPlan({updateMealsID:setMealsID})
-const navigate = useNavigate()
+  const [minCalories, setMinCalories] = useState('')
+  const [maxCalories, setMaxCalories] = useState('')
+  const { setMealsID } = useContext(Context)
+  const [checkedIDDiet, setCheckedIDDiet] = useState([])
+  const [checkedIDAllergies, setCheckedIDAllergies] = useState([])
+  const dietHealth = useChecked({ checkedType: checkedIDDiet, checkedList: dietType })
+  const allergiesCare = useChecked({ checkedType: checkedIDAllergies, checkedList: allergies })
+  const { params, error } = useDietParams({ minCalories, maxCalories, dietHealth, allergiesCare })
+  const { planResult } = useDietPlan({ updateMealsID: setMealsID })
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(error) return
-    if(!params) return
+    if (error) return
+    if (!params) return
     planResult(params)
-    navigate("/plan")
-    
+    navigate('/plan')
   }
-  
- const handleChangeDiet =  (e) =>{
-  if(e.target.checked){
-    setCheckedIDDiet([...checkedIDDiet,e.target.id])
-  }else{
-    setCheckedIDDiet(checkedIDDiet.filter(id => id != e.target.id))
-  }
- }
 
- const handleChangeAllergies =  (e) => {
-  if(e.target.checked){
-    setCheckedIDAllergies([...checkedIDAllergies,e.target.id])
-  }else{
-    setCheckedIDAllergies(checkedIDAllergies.filter(id => id != e.target.id))
+  const handleChangeDiet = (e) => {
+    if (e.target.checked) {
+      setCheckedIDDiet([...checkedIDDiet, e.target.id])
+    } else {
+      setCheckedIDDiet(checkedIDDiet.filter(id => id != e.target.id))
+    }
   }
- }
-console.log(checkedIDDiet)
-console.log(checkedIDAllergies)
-console.log(dietHealth)
-console.log(allergiesCare)
+
+  const handleChangeAllergies = (e) => {
+    if (e.target.checked) {
+      setCheckedIDAllergies([...checkedIDAllergies, e.target.id])
+    } else {
+      setCheckedIDAllergies(checkedIDAllergies.filter(id => id != e.target.id))
+    }
+  }
+  console.log(checkedIDDiet)
+  console.log(checkedIDAllergies)
+  console.log(dietHealth)
+  console.log(allergiesCare)
 
   return (
     <>
       <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-8 py-8 ">
         <h5 className="font-poppins font-semibold text-xl text-center text-primary">DIET PLAN SPECIFICATIONS</h5>
-        <div className="flex flex-col md:flex-row items-center justify-center gap-8">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 p-4 sm:p-0">
           <div className="flex  items-center justify-center gap-2">
             <label htmlFor="minCalories" className="font-poppins font-bold">minimum caloric value(kcal):</label>
             <input type="number" id="minCalories" onChange={(e) => setMinCalories(e.target.value)} value={minCalories} required className="border border-primary outline-none focus:shadow-primary font-poppins text-center w-[100px] focus:shadow-sm rounded"></input>
           </div>
           <div className="flex items-center justify-center gap-2">
             <label htmlFor="maxCalories" className="font-poppins font-bold">maximum caloric value(kcal):</label>
-            <input type="number" id="maxCalories" onChange={(e) => setMaxCalories(e.target.value)} value={maxCalories}  required className="border border-primary outline-none focus:shadow-primary font-poppins w-[100px] text-center focus:shadow-sm rounded"></input>
+            <input type="number" id="maxCalories" onChange={(e) => setMaxCalories(e.target.value)} value={maxCalories} required className="border border-primary outline-none focus:shadow-primary font-poppins w-[100px] text-center focus:shadow-sm rounded"></input>
           </div>
         </div>
         {error && <p className="text-[red] font-poppins text-center  text-bold px-4">{error}</p>}
@@ -82,12 +80,12 @@ console.log(allergiesCare)
               allergies.map(value =>
                 <div key={value.id} className="flex items-center justify-between w-[200px] mt-2 mr-12">
                   <label htmlFor={value.id} className="font-poppins font-semibold">{value.name}</label>
-                  <input type="checkbox" id={value.id} onChange={handleChangeAllergies}  />
+                  <input type="checkbox" id={value.id} onChange={handleChangeAllergies} />
                 </div>
               )
             }
             </div>
-            
+
           </div>
         </div>
 
